a1b8fd31da67ca5a70add1c818de822400549afd
Rick Cohen <rickco@gmail.com>
2020-12-26 14:46:57 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(".icon").hover(function() {
+    $(this).toggleClass("icon-large");
+  });
+});
+
+function loadRepo() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      results = JSON.parse(this.responseText);
+      for (let i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+        let ul = document.getElementById("repositories");
+        let li = document.createElement("LI");
+        li.className = "list-group-item";
+        let a = document.createElement("a");
+
+        li.appendChild(document.createTextNode(results[i].name));
+        a.appendChild(li);
+        a.setAttribute("href", results[i].html_url);
+        ul.appendChild(a);
+      }
+    }
+  };
+  xhttp.open("GET", "https://api.github.com/users/STUDENTUSERNAME/repos", true);
+  xhttp.send();
+}
